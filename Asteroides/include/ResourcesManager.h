ebbe0404c29a5112933de52ed705f41841534dfd
Emilie Schott <emilie.schott@gmail.com>
2024-05-18 20:37:23 +0200

    Modify ResourcesManager class to make a template and manage soundbuffer and texture

--- Asteroides/include/ResourcesManager.h
+++ Asteroides/include/ResourcesManager.h
@@ -1,17 +1,27 @@
 #pragma once
 
-#include <SFML/Graphics.hpp>
+#include <iostream>
 #include <string_view>
 #include <unordered_map>
+#include "../include/MissingResourceException.h"
 
-
+template <typename T> 
 class ResourcesManager
 {
 	public:
 		ResourcesManager() = delete;
-		static sf::Texture const& getResource(std::string_view const& imagePath);
+		static T const& getResource(std::string_view const& imagePath) {
+			auto result = resources.find(imagePath);
+			if (result == end(resources)) {
+				if (resources[imagePath].loadFromFile(imagePath.data()) == false) {
+					throw MissingResourceException{ imagePath };
+				}
+				return resources[imagePath];
+			}
+			return result->second;
+		}
 
 	private :
-		static std::unordered_map<std::string_view, sf::Texture> resources;
+		static std::unordered_map<std::string_view, T> resources;
 };
 
--- Asteroides/src/ResourcesManager.cpp
+++ Asteroides/src/ResourcesManager.cpp
@@ -1,21 +1,12 @@
-#include <iostream>
+
 #include <SFML/Graphics.hpp>
-#include <string_view>
-#include <unordered_map>
+#include <SFML/Audio.hpp>
 #include "../include/ResourcesManager.h"
-#include "../include/MissingResourceException.h"
 
 using namespace std;
 
-unordered_map<string_view, sf::Texture> ResourcesManager::resources{};
+template<>
+unordered_map<string_view, sf::Texture> ResourcesManager<sf::Texture>::resources{};
 
-sf::Texture const& ResourcesManager::getResource(string_view const& imagePath) {
-	auto result = resources.find(imagePath);
-	if (result == end(resources)) {
-		if (resources[imagePath].loadFromFile(imagePath.data()) == false) {
-			throw MissingResourceException{ imagePath };
-		}
-		return resources[imagePath];
-	}
-	return result->second;
-}
+template<>
+unordered_map<string_view, sf::SoundBuffer> ResourcesManager<sf::SoundBuffer>::resources{};
